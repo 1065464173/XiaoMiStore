2d1565a505a2b0664c4981f5b115888b53d3f0f0
sxuet <1065464173@qq.com>
2022-03-09 15:04:11 +0800

    commit

--- src/Util/ConnectionPool.java
+++ src/Util/ConnectionPool.java
@@ -3,11 +3,14 @@ package Util;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionPool {
-    public static DataSource getDataSourceByC3P0ByXML() {
+    public static DataSource getDataSourceByC3P0ByXML() throws SQLException {
         ComboPooledDataSource c3p0 = new ComboPooledDataSource("xiaomistore");
-
+        Connection connection = DriverManager.getConnection("");
         return c3p0;
     }
 }
